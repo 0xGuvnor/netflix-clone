cb294ac1190ee09d933dbe03f28026910993116e
0xpaprhnds <101540063+0xpaprhnds@users.noreply.github.com>
2022-12-06 01:21:15 +0800

    Section Rows init

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,7 +1,51 @@
-import React from "react";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import { baseUrl } from "../constants/movie";
+import { Movie } from "../typings";
+import { FaPlay } from "react-icons/fa";
+import { IoMdInformationCircleOutline } from "react-icons/io";
 
-const Banner = () => {
-  return <div>Banner</div>;
+interface Props {
+  trendingNow: Movie[];
+}
+
+const Banner = ({ trendingNow }: Props) => {
+  const [movie, setMovie] = useState<Movie | null>(null);
+
+  useEffect(() => {
+    setMovie(trendingNow[Math.floor(Math.random() * trendingNow.length)]);
+  }, [trendingNow]);
+
+  return (
+    <div className="flex flex-col py-16 space-y-2 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
+      <div className="absolute top-0 left-0 h-[95vh] w-screen -z-10">
+        <Image
+          src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
+          alt="Movie Banner"
+          fill
+          className="object-cover"
+        />
+      </div>
+
+      <h1 className="text-2xl font-bold text-shadow-md lg:text-7xl md:text-4xl">
+        {movie?.title || movie?.name || movie?.original_name}
+      </h1>
+      <p className="max-w-xs text-xs text-shadow-md md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">
+        {movie?.overview}
+      </p>
+
+      <div className="flex max-w-xs space-x-3 md:max-w-lg">
+        <button className="w-1/2 text-black bg-white bannerBtn">
+          <FaPlay className="w-4 h-4 text-black md:w-7 md:h-7" />
+          Play
+        </button>
+        <button className="bannerBtn bg-[gray]/70 w-1/2">
+          <IoMdInformationCircleOutline className="w-5 h-5 md:h-8 md:w-8" />
+          More Info
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
---
+++ utils/requests.ts
@@ -0,0 +1,15 @@
+const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+const baseUrl = "https://api.themoviedb.org/3";
+
+const requests = {
+  fetchTrending: `${baseUrl}/trending/all/week?api_key=${apiKey}&language=en-US`,
+  fetchNetflixOriginals: `${baseUrl}/discover/movie?api_key=${apiKey}&with_networks=213`,
+  fetchTopRated: `${baseUrl}/movie/top_rated?api_key=${apiKey}&language=en-US`,
+  fetchActionMovies: `${baseUrl}/discover/movie?api_key=${apiKey}&language=en-US&with_genres=28`,
+  fetchComedyMovies: `${baseUrl}/discover/movie?api_key=${apiKey}&language=en-US&with_genres=35`,
+  fetchHorrorMovies: `${baseUrl}/discover/movie?api_key=${apiKey}&language=en-US&with_genres=27`,
+  fetchRomanceMovies: `${baseUrl}/discover/movie?api_key=${apiKey}&language=en-US&with_genres=10749`,
+  fetchDocumentaries: `${baseUrl}/discover/movie?api_key=${apiKey}&language=en-US&with_genres=99`,
+};
+
+export default requests;
