da1de1e4fa0e333163ee0f547f3b1bb34d99878b
0xpaprhnds <101540063+0xpaprhnds@users.noreply.github.com>
2022-12-05 09:31:45 +0800

    init

---
+++ components/Header.tsx
@@ -0,0 +1,66 @@
+import Image from "next/image";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import { HiSearch, HiBell } from "react-icons/hi";
+
+const Header = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <header
+      className={`${
+        isScrolled && "bg-[#141414] transition duration-300 ease-in-out"
+      }`}
+    >
+      <div className="flex items-center space-x-2 md:space-x-10">
+        <Image
+          src="https://rb.gy/ulxxee"
+          alt="Netflix Logo"
+          width={100}
+          height={100}
+          className="object-contain cursor-pointer"
+        />
+        <ul className="hidden space-x-4 md:flex">
+          <li className="headerLink">Home</li>
+          <li className="headerLink">Series</li>
+          <li className="headerLink">Films</li>
+          <li className="headerLink">New & Popular</li>
+          <li className="headerLink">My List</li>
+        </ul>
+      </div>
+
+      <div className="flex items-center space-x-4 text-sm font-light">
+        <HiSearch className="hidden w-6 h-6 sm:inline" />
+        <p className="hidden lg:inline">Kids</p>
+        <HiBell className="w-6 h-6" />
+        <Link href="/account">
+          <Image
+            src="https://rb.gy/g1pwyx"
+            alt="Profile Picture"
+            width={28}
+            height={28}
+            className="rounded cursor-pointer"
+          />
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
