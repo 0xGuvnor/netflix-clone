8b5a19b606f59e2f889395cb510c8664e98db18f
0xpaprhnds <101540063+0xpaprhnds@users.noreply.github.com>
2022-12-06 03:03:12 +0800

    bug fixing banner image

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,8 +27,6 @@ const Home: NextPage<Props> = ({
   romanceMovies,
   documentaries,
 }) => {
-  console.log(actionMovies[0]);
-
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
