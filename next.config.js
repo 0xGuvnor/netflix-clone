7e3f7b2829cef9f078a6accb36cfe0701383a48a
0xpaprhnds <101540063+0xpaprhnds@users.noreply.github.com>
2022-12-06 14:25:54 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,5 +5,13 @@ module.exports = {
     domains: ["rb.gy", "image.tmdb.org"],
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+        port: "",
+        pathname: "/**",
+      },
+    ],
   },
 };
