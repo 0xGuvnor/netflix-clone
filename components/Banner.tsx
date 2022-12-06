444b9d5ea239bbfdf25872ffa572d7e5eeb2f836
0xpaprhnds <101540063+0xpaprhnds@users.noreply.github.com>
2022-12-06 12:18:40 +0800

    Update Banner.tsx

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -18,13 +18,13 @@ const Banner = ({ trendingNow }: Props) => {
 
   return (
     <div className="flex flex-col py-16 space-y-2 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
-      <div className="absolute top-0 left-0 h-[95vh] w-screen -z-10 overflow-hidden">
+      <div className="absolute top-0 left-0 h-[95vh] w-screen -z-10">
         {movie && (
           <Image
             src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
             alt="Movie Banner"
             fill
-            className="object-cover"
+            className="object-cover overflow-hidden"
           />
         )}
       </div>
