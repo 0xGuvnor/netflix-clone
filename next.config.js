cecccf3a83df46970edeb9835a6269da3c73c3d1
0xpaprhnds <101540063+0xpaprhnds@users.noreply.github.com>
2022-12-06 14:34:22 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,15 +2,21 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["rb.gy", "image.tmdb.org"],
+    // domains: ["rb.gy", "image.tmdb.org"],
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "**.tmdb.org",
+        hostname: "image.tmdb.org",
         // port: "",
-        // pathname: "/**",
+        pathname: "/t/p/**",
+      },
+      {
+        protocol: "https",
+        hostname: "rb.gy",
+        // port: "",
+        pathname: "/**",
       },
     ],
   },
