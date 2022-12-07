23d03ec4edf1465057408a66e7072c9ba61db93d
0xpaprhnds <101540063+0xpaprhnds@users.noreply.github.com>
2022-12-08 02:34:47 +0800

    Update Modal.tsx

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -108,7 +108,7 @@ const Modal = () => {
           </div>
         </div>
 
-        <div className="flex space-x-16 rounded-b-md bg-[#181818] px-10 py-8">
+        <div className="flex space-x-16 rounded-b-xl bg-[#181818] px-10 py-8">
           <div className="space-y-6 text-lg">
             <div className="flex items-center space-x-2 text-sm">
               <p className="font-semibold text-green-400">
