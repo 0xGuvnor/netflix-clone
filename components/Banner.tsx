677f96ae2f83e8b6eb876e72645eb1f6bccfb890
0xpaprhnds <101540063+0xpaprhnds@users.noreply.github.com>
2022-12-08 04:35:05 +0800

    Update Banner.tsx

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -22,7 +22,7 @@ const Banner = ({ trendingNow }: Props) => {
 
   return (
     <div className="flex flex-col py-16 space-y-2 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
-      <div className="absolute top-0 left-0 h-[95vh] w-screen -z-10">
+      <div className="absolute top-0 left-0 h-[95vh] w-screen -zz-10">
         <Image
           src={`https://image.tmdb.org/t/p/w500${
             movie?.backdrop_path || movie?.poster_path
@@ -37,32 +37,33 @@ const Banner = ({ trendingNow }: Props) => {
           sizes="100vw"
           priority
           quality={100}
-          className="z-10 object-cover"
+          className="object-cover"
         />
       </div>
+      <div className="z-20">
+        <h1 className="text-2xl max-w-[75%] font-bold text-shadow-md lg:text-5xl md:text-4xl">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <p className="max-w-xs text-xs text-justify text-shadow-md md:max-w-lg md:text-md lg:max-w-2xl lg:text-lg">
+          {movie?.overview}
+        </p>
 
-      <h1 className="text-2xl max-w-[75%] font-bold text-shadow-md lg:text-5xl md:text-4xl">
-        {movie?.title || movie?.name || movie?.original_name}
-      </h1>
-      <p className="max-w-xs text-xs text-justify text-shadow-md md:max-w-lg md:text-md lg:max-w-2xl lg:text-lg">
-        {movie?.overview}
-      </p>
-
-      <div className="flex max-w-xs space-x-3 md:max-w-lg">
-        <button className="w-1/2 text-black bg-white bannerBtn">
-          <FaPlay className="w-4 h-4 text-black md:w-7 md:h-7" />
-          Play
-        </button>
-        <button
-          onClick={() => {
-            setShowModal(true);
-            setModalMovie(movie);
-          }}
-          className="bannerBtn bg-[gray]/70 w-1/2"
-        >
-          <IoMdInformationCircleOutline className="w-5 h-5 md:h-8 md:w-8" />
-          More Info
-        </button>
+        <div className="flex max-w-xs space-x-3 md:max-w-lg">
+          <button className="w-1/2 text-black bg-white bannerBtn">
+            <FaPlay className="w-4 h-4 text-black md:w-7 md:h-7" />
+            Play
+          </button>
+          <button
+            onClick={() => {
+              setShowModal(true);
+              setModalMovie(movie);
+            }}
+            className="bannerBtn bg-[gray]/70 w-1/2"
+          >
+            <IoMdInformationCircleOutline className="w-5 h-5 md:h-8 md:w-8" />
+            More Info
+          </button>
+        </div>
       </div>
     </div>
   );
